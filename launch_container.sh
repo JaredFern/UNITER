1c7260f5619d7b7031eb63b532ca501670ff3799
Jared Fernandez <jaredfern@cmu.edu>
2021-01-27 20:19:07 -0500

    Local paths for custom docker

--- launch_container.sh
+++ launch_container.sh
@@ -1,10 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-TXT_DB=$1
-IMG_DIR=$2
-OUTPUT=$3
-PRETRAIN_DIR=$4
+TXT_DB="/home/jaredfer/data/txt_db"
+IMG_DIR="/home/jaredfer/data/img_db"
+OUTPUT="/home/jaredfer/data/storage"
+PRETRAIN_DIR="/home/jaredfer/data/pretrained"
 
 if [ -z $CUDA_VISIBLE_DEVICES ]; then
     CUDA_VISIBLE_DEVICES='all'
@@ -18,4 +18,4 @@ docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
     --mount src=$TXT_DB,dst=/txt,type=bind,readonly \
     --mount src=$IMG_DIR,dst=/img,type=bind,readonly \
     -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
-    -w /src chenrocks/uniter
+    -w /src jaredfern/uniter /usr/bin/zsh
