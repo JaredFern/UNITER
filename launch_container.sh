8cac5a88e2e8c4c91e5a750162ea5071486dfe75
Jared Fernandez <jaredfern@cmu.edu>
2021-01-29 17:15:18 -0500

    update launch_container for herron paths

--- launch_container.sh
+++ launch_container.sh
@@ -1,13 +1,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-TXT_DB=/home/jaredfer/data/txt_db
-IMG_DIR=/home/jaredfer/data/img_db
-OUTPUT=/home/jaredfer/data/storage
-PRETRAIN_DIR=/home/jaredfer/data/pretrained
+TXT_DB="/home/jaredfer/data/datasets/txt_db"
+IMG_DIR="/home/jaredfer/data/datasets/img_db"
+CORPORA_DIR="/home/jaredfer/data/corpora"
+OUTPUT="/home/jaredfer/data/models/storage"
+PRETRAIN_DIR="/home/jaredfer/data/models/pretrained"
 
 if [ -z $CUDA_VISIBLE_DEVICES ]; then
-    CUDA_VISIBLE_DEVICES='ALL'
+    CUDA_VISIBLE_DEVICES='all'
 fi
 
 
@@ -15,7 +16,8 @@ docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
     --mount src=$(pwd),dst=/src,type=bind \
     --mount src=$OUTPUT,dst=/storage,type=bind \
     --mount src=$PRETRAIN_DIR,dst=/pretrain,type=bind,readonly \
-    --mount src=$TXT_DB,dst=/txt,type=bind,readonly \
-    --mount src=$IMG_DIR,dst=/img,type=bind,readonly \
+    --mount src=$TXT_DB,dst=/txt,type=bind \
+    --mount src=$IMG_DIR,dst=/img,type=bind \
+    --mount src=$CORPORA_DIR,dst=/corpora,type=bind,readonly \
     -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
-    -w /src jaredfern/uniter
+    -w /src jaredfern/uniter /usr/bin/zsh
